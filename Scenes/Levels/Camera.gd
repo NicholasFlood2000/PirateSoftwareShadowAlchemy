727d0df2937a7881697f11ba49b3db1ce31c1b81
Mousedotexecutable <140970703+Mousedotexecutable@users.noreply.github.com>
2024-07-20 11:57:35 +0200

    Stuff (#2)

---
+++ Scenes/Levels/Camera.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+#
+#func _physics_process(delta):
+	#var movement_input :Vector2 = Vector2.ZERO
+	#
+	#
+	#movement_input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down", )
+	#
+	#position += (movement_input * 200) * delta
