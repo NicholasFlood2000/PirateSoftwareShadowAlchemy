2fcf73cb83017a31199f1e72b4c4417b083866b3
NicholasFlood2000 <nicholas.flood2000@gmail.com>
2024-07-22 19:01:16 -0400

    Adds Tree Platforms
    Adds tree modulation and platforms. Replaces fire background to earth background

---
+++ Scripts/Colors.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var ShadowColor: Color = Color.hex(0x1a1a1aFF)
+var EarthColor: Color = Color.hex(0x00FF00FF)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
