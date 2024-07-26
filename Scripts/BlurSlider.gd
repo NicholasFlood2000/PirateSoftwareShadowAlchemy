735b1b49288d20cec7935cafe48820a47f54adc7
NicholasFlood2000 <nicholas.flood2000@gmail.com>
2024-07-26 11:24:23 -0400

    Adds Air Background and Blur Slider
    Changes the Change Background script to work with the Air Background and implements the Blur Slider.

--- Scripts/BlurSlider.gd
+++ Scripts/BlurSlider.gd
@@ -4,7 +4,7 @@ extends ColorRect
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GlobalSettings.BlurChanged.connect(UpdateBlur)
-	self.material.set("shader_parameter/lod_value", 1)
+	self.material.set("shader_parameter/lod_value", GlobalSettings.Blur)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,5 +12,4 @@ func _process(delta):
 	pass
 
 func UpdateBlur():
-	#Code to update blur
-	pass
+	material.set("shader_parameter/lod_value", GlobalSettings.Blur)
--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -10,6 +10,10 @@ extends Node2D
 @onready var WaterBackgroundBack = $WaterBackground/WaterBackground2
 @onready var WaterBackgroundMiddle = $WaterBackground/ParallaxLayer2/Sprite2D
 @onready var WaterBackgroundFront = $WaterBackground/ParallaxLayer3/Sprite2D
+@onready var AirBackgroundMain = $AirBackground
+@onready var AirBackgroundBack = $AirBackground/AirBackground2
+@onready var AirBackgroundMiddle = $AirBackground/ParallaxLayer2/Sprite2D
+@onready var AirBackgroundFront = $AirBackground/ParallaxLayer3/Sprite2D
 
 signal BackgroundChanged(NewBackground)
 
@@ -17,12 +21,23 @@ var LayerNum = -100
 var CurrentBackground = "Water"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	ScreenTransition.transition_out()
 	
-	
 	CurrentBackground = InitialBackground
-	BackgroundChanged.emit( CurrentBackground )
+	BackgroundChanged.emit(CurrentBackground)
+	match(CurrentBackground):
+		"Air":
+			AirBackgroundBack.visible = true
+			AirBackgroundMiddle.visible = true
+			AirBackgroundFront.visible = true
+		"Earth":
+			EarthBackgroundBack.visible = true
+			EarthBackgroundMiddle.visible = true
+			EarthBackgroundFront.visible = true
+		"Water":
+			WaterBackgroundBack.visible = true
+			WaterBackgroundMiddle.visible = true
+			WaterBackgroundFront.visible = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -48,6 +63,18 @@ func toggleBackGround(OldBackground, NewBackground):
 			EarthBackgroundFront.visible = false
 			WaterBackgroundFront.visible = true
 			CurrentBackground = "Water"
+		elif(NewBackground == "Air"):
+			EarthBackgroundMain.layer = LayerNum
+			AirBackgroundMain.layer = LayerNum-1
+			EarthBackgroundBack.visible = false
+			AirBackgroundBack.visible = true
+			await get_tree().create_timer(1).timeout
+			EarthBackgroundMiddle.visible = false
+			AirBackgroundMiddle.visible = true
+			await get_tree().create_timer(1).timeout
+			EarthBackgroundFront.visible = false
+			AirBackgroundFront.visible = true
+			CurrentBackground = "Air"
 	elif(OldBackground == "Water"):
 		if(NewBackground == "Earth"):
 			WaterBackgroundMain.layer = LayerNum
@@ -59,6 +86,43 @@ func toggleBackGround(OldBackground, NewBackground):
 			EarthBackgroundMiddle.visible = true
 			await get_tree().create_timer(1).timeout
 			WaterBackgroundFront.visible = false
+			EarthBackgroundFront.visible = true
+			CurrentBackground = "Earth"
+		elif(NewBackground == "Air"):
+			WaterBackgroundMain.layer = LayerNum
+			AirBackgroundMain.layer = LayerNum-1
+			WaterBackgroundBack.visible = false
+			AirBackgroundBack.visible = true
+			await get_tree().create_timer(1).timeout
+			WaterBackgroundMiddle.visible = false
+			AirBackgroundMiddle.visible = true
+			await get_tree().create_timer(1).timeout
+			WaterBackgroundFront.visible = false
+			AirBackgroundFront.visible = true
+			CurrentBackground = "Air"
+	elif(OldBackground == "Air"):
+		if(NewBackground == "Water"):
+			AirBackgroundMain.layer = LayerNum
+			WaterBackgroundMain.layer = LayerNum-1
+			AirBackgroundBack.visible = false
+			WaterBackgroundBack.visible = true
+			await get_tree().create_timer(1).timeout
+			AirBackgroundMiddle.visible = false
+			WaterBackgroundMiddle.visible = true
+			await get_tree().create_timer(1).timeout
+			AirBackgroundFront.visible = false
+			WaterBackgroundFront.visible = true
+			CurrentBackground = "Water"
+		elif(NewBackground == "Earth"):
+			AirBackgroundMain.layer = LayerNum
+			EarthBackgroundMain.layer = LayerNum-1
+			AirBackgroundBack.visible = false
+			EarthBackgroundBack.visible = true
+			await get_tree().create_timer(1).timeout
+			AirBackgroundMiddle.visible = false
+			EarthBackgroundMiddle.visible = true
+			await get_tree().create_timer(1).timeout
+			AirBackgroundFront.visible = false
 			EarthBackgroundFront.visible = true
 			CurrentBackground = "Earth"
 	
