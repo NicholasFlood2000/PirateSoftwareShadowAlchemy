862b64e7a346a3b72ceae9e42107532c90696a41
NicholasFlood2000 <nicholas.flood2000@gmail.com>
2024-07-26 12:43:08 -0400

    Moving Platform Framework
    Adds a framework for moving platforms

--- Scripts/LevelProgression.gd
+++ Scripts/LevelProgression.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var Levels = ["res://Scenes/Levels/Level_2.tscn"]
+var Levels = ["res://Scenes/Levels/Level_2.tscn", "res://Scenes/Levels/Level_3.tscn"]
 var CurrentLevel = 0
 
 #This does not count Level 1 because Play Button Handles Level 1
