b7e6cadee0a89a41ea75a3eba9aa03a7df7aabc2
Mousedotexecutable <140970703+Mousedotexecutable@users.noreply.github.com>
2024-07-26 16:41:38 +0200

    Added Screen transition code (#12)

---
+++ Scenes/ScreenTransition.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+func transition_in():
+	var tween = get_tree().create_tween()
+	$ColorRect.modulate.a = 0
+	
+	tween.tween_property($ColorRect, "modulate", Color(1,1,1,1), 1.0).set_trans(Tween.TRANS_CUBIC)
+	
+	await tween.finished
+
+func transition_out():
+	var tween = get_tree().create_tween()
+	$ColorRect.modulate.a = 1
+	
+	tween.tween_property($ColorRect, "modulate", Color(1,1,1,0.0), 1.0).set_trans(Tween.TRANS_CUBIC)
