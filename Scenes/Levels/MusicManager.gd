0a3927eff78a2578a36ec5a7e7e2e87f43ca4e6b
MouseDotExecutable <blasermichael3@gmail.com>
2024-07-29 16:59:27 +0200

    Added the transitions for songs

--- Scenes/Levels/MusicManager.gd
+++ Scenes/Levels/MusicManager.gd
@@ -20,18 +20,46 @@ func _process(delta):
 func _BackgroundChanged(NewBackground):
 	match(NewBackground):
 		"Air":
+			#$Water.
+			var tween = get_tree().create_tween()
+			tween.tween_property(Earth, "volume_db" , -80, 2.0).set_trans(Tween.TRANS_CUBIC)
+			tween.tween_property(Water, "volume_db" , -80, 2.0).set_trans(Tween.TRANS_CUBIC)
+			await tween.finished
+			tween.kill()
+			
 			Water.playing = false
 			if(Earth.playing):
 				Earth.playing = false
 			if(not Air.playing):
 				Air.play()
+				tween = get_tree().create_tween()
+				tween.tween_property(Air, "volume_db" , 0.0 , 2.0).set_trans(Tween.TRANS_CUBIC)
+				
 		"Water":
+			var tween = get_tree().create_tween()
+			tween.tween_property(Earth, "volume_db" , -80, 2.0).set_trans(Tween.TRANS_CUBIC)
+			tween.tween_property(Air, "volume_db" , -80, 2.0).set_trans(Tween.TRANS_CUBIC)
+			await tween.finished
+			tween.kill()
+			
 			if(not Water.playing):
 				Water.play()
+				tween = get_tree().create_tween()
+				tween.tween_property(Water, "volume_db" , 0, 2.0).set_trans(Tween.TRANS_CUBIC)
 			Earth.playing = false
 			Air.playing = false
 		"Earth":
+			var tween = get_tree().create_tween()
+			tween.tween_property(Water, "volume_db" , -80, 2.0).set_trans(Tween.TRANS_CUBIC)
+			tween.tween_property(Air, "volume_db" , -80, 2.0).set_trans(Tween.TRANS_CUBIC)
+			await tween.finished
+			tween.kill()
+			
 			Water.playing = false
 			if(not Earth.playing):
 				Earth.play()
+				tween = get_tree().create_tween()
+				tween.tween_property(Earth, "volume_db" , 0, 2.0).set_trans(Tween.TRANS_CUBIC)
+				#Earth.volume_db = 0
+				
 			Air.playing = false
