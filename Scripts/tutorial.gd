ace5bcc623da0d773d76a57b007b5898cce19988
NicholasFlood2000 <nicholas.flood2000@gmail.com>
2024-07-29 16:34:10 -0400

    Adds Dog NPC for tutorial and polishes Water
    Adds Dog NPC for tutorials and polishes alpha on water color.

--- Scripts/tutorial.gd
+++ Scripts/tutorial.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var Screen = $Screen
 
+var PlayerInside = false
 var Visited: bool = false
 
 # Called when the node enters the scene tree for the first time.
@@ -11,13 +12,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-
-func _on_area_2d_body_entered(body):
-	if(body.name == "Player" and (not Visited)):
+	if(Input.is_action_just_pressed("Interact") and (not Visited) and PlayerInside):
 		Visited = true
 		Screen.process_mode = Node.PROCESS_MODE_ALWAYS
 		Screen.visible = true
 		get_tree().paused = true
-		print("Paused Player")
+
+
+func _on_area_2d_body_entered(body):
+	if(body.name == "Player"):
+		PlayerInside = true
+
+
+func _on_area_2d_body_exited(body):
+	if(body.name == "Player"):
+		PlayerInside = false
